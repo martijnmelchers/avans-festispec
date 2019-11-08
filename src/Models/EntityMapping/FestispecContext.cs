ece6be4ddb808efffc5a2936e300e4933e01bfc0
NanoSector <rick@nanosector.nl>
2019-11-08 19:38:31 +0100

    Make all DbSets virtual

--- src/Models/EntityMapping/FestispecContext.cs
+++ src/Models/EntityMapping/FestispecContext.cs
@@ -15,25 +15,25 @@ namespace Festispec.Models.EntityMapping
             modelBuilder.Configurations.AddFromAssembly(typeof(FestispecContext).Assembly);
         }
 
-        public DbSet<Account> Accounts { get; set; }
-        public DbSet<Address> Addresses { get; set; }
-        public DbSet<Answer> Answers { get; set; }
-        public DbSet<Attachment> Attachments { get; set; }
-        public DbSet<Availability> Availabilities { get; set; }
-        public DbSet<Certificate> Certificates { get; set; }
-        public DbSet<ContactDetails> ContactDetails { get; set; }
-        public DbSet<ContactPerson> ContactPersons { get; set; }
-        public DbSet<ContactPersonNote> ContactPersonNotes { get; set; }
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Employee> Employees { get; set; }
-        public DbSet<Festival> Festivals { get; set; }
-        public DbSet<OpeningHours> OpeningHours { get; set; }
-        public DbSet<PlannedEvent> PlannedEvents { get; set; }
-        public DbSet<PlannedInspection> PlannedInspections { get; set; }
-        public DbSet<Question> Questions { get; set; }
-        public DbSet<QuestionCategory> QuestionCategories { get; set; }
-        public DbSet<Questionnaire> Questionnaires { get; set; }
-        public DbSet<Report> Reports { get; set; }
-        public DbSet<ReportEntry> ReportEntries { get; set; }
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Address> Addresses { get; set; }
+        public virtual DbSet<Answer> Answers { get; set; }
+        public virtual DbSet<Attachment> Attachments { get; set; }
+        public virtual DbSet<Availability> Availabilities { get; set; }
+        public virtual DbSet<Certificate> Certificates { get; set; }
+        public virtual DbSet<ContactDetails> ContactDetails { get; set; }
+        public virtual DbSet<ContactPerson> ContactPersons { get; set; }
+        public virtual DbSet<ContactPersonNote> ContactPersonNotes { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<Festival> Festivals { get; set; }
+        public virtual DbSet<OpeningHours> OpeningHours { get; set; }
+        public virtual DbSet<PlannedEvent> PlannedEvents { get; set; }
+        public virtual DbSet<PlannedInspection> PlannedInspections { get; set; }
+        public virtual DbSet<Question> Questions { get; set; }
+        public virtual DbSet<QuestionCategory> QuestionCategories { get; set; }
+        public virtual DbSet<Questionnaire> Questionnaires { get; set; }
+        public virtual DbSet<Report> Reports { get; set; }
+        public virtual DbSet<ReportEntry> ReportEntries { get; set; }
     }
 }
