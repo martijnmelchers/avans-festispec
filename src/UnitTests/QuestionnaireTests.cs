d14965d0ce64d910528e865f5cb287ae4ffda160
ThaDennuz <dgghaare@avans.nl>
2019-11-08 18:17:58 +0100

    Added templates for features in vragenlijst-beheren

---
+++ src/UnitTests/QuestionnaireTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Festispec.UnitTests
+{
+    public class QuestionnaireTests
+    {
+        [Fact]
+        public void AddingQuestionnaire()
+        {
+            Assert.True(false);
+        }
+        [Fact]
+        public void RemovingQuestionnaire()
+        {
+            Assert.True(false);
+        }
+        [Fact]
+        public void AddingStringQuestion()
+        {
+            Assert.True(false);
+        }
+        [Fact]
+        public void AddingMultipleChoiceQuestion()
+        {
+            Assert.True(false);
+        }
+        [Fact]
+        public void AddingDrawQuestion()
+        {
+            Assert.True(false);
+        }
+        [Fact]
+        public void AddingNumberQuestion()
+        {
+            Assert.True(false);
+        }
+        [Fact]
+        public void AddingBooleanQuestion()
+        {
+            Assert.True(false);
+        }
+        [Fact]
+        public void AddingImageQuestion()
+        {
+            Assert.True(false);
+        }
+        [Fact]
+        public void RemovingQuestion()
+        {
+            Assert.True(false);
+        }
+    }
+}
