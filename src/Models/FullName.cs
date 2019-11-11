711567ba305f39d878e43b932b20c4bcc71b3d96
Martijn <martijn@melchers.nu>
2019-11-11 14:47:14 +0100

    Add FullName

---
+++ src/Models/FullName.cs
@@ -0,0 +1,14 @@
+﻿namespace Festispec.Models
+{
+    public class FullName
+    {
+        public string First { get; set; }
+        public string Middle { get; set; }
+        public string Last { get; set; }
+        public override string ToString()
+        {
+            return string.IsNullOrEmpty(Middle) ? $"{First} {Last}" : $"{First} {Middle} {Last}";
+        }
+    }
+}
+
