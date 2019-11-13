df808b8befd50975d50a5879c818d0c3f9229920
stijntoemen <stijntoemen@gmail.com>
2019-11-13 14:09:22 +0100

    Added Refrence question

---
+++ src/Models/ReferenceQuestion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Festispec.Models
+{
+    public class ReferenceQuestion : Questions.Question
+    {
+        public Questions.Question Question { get; set; }
+
+        public override GraphType GraphType => Question.GraphType;
+    }
+}
