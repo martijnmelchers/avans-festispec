8dd1caeb3d6e6e3efca9511db1341ad6b6537073
NanoSector <rick@nanosector.nl>
2019-11-19 19:06:52 +0100

    Update seed data
    Signed-off-by: NanoSector <rick@nanosector.nl>

--- src/Models/Migrations/Configuration.cs
+++ src/Models/Migrations/Configuration.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections.Generic;
+using Festispec.Models.EntityMapping;
 
 namespace Festispec.Models.Migrations
 {
     using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Festispec.Models.EntityMapping.FestispecContext>
     {
@@ -21,7 +20,137 @@ namespace Festispec.Models.Migrations
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
 
-            var employee = new Employee() {
+            var employee = CreateEmployee(context);
+
+            var customer = new Customer()
+            {
+                Id = 1,
+                CustomerName = "Q-DANCE",
+                KvkNr = 34212891,
+                Address = new Address
+                {
+                    Id = 2,
+                    StreetName = "Isolatorweg",
+                    HouseNumber = 36,
+                    ZipCode = "1014AS",
+                    City = "Amsterdam",
+                    Country = "Nederland"
+                },
+                ContactDetails = new ContactDetails
+                {
+                    Id = 2,
+                    EmailAddress = "info@q-dance.com",
+                    PhoneNumber = "+31204877300"
+                }
+            };
+
+            context.Customers.AddOrUpdate(customer);
+
+            var contactDetails = new ContactDetails
+            {
+                Id = 3,
+
+                // fake news
+                EmailAddress = "nielskijf@q-dance.com"
+            };
+
+            context.ContactDetails.AddOrUpdate(contactDetails);
+
+            var fullName = new FullName
+            {
+                Id = 2,
+                First = "Niels",
+                Last = "Kijf"
+            };
+
+            context.FullNames.AddOrUpdate(fullName);
+
+            var contactPerson = new ContactPerson()
+            {
+                Id = 1,
+                Customer = customer,
+                Name = fullName,
+                ContactDetails = contactDetails,
+                Role = "MA"
+            };
+
+            context.ContactPersons.AddOrUpdate(contactPerson);
+
+            var note = new ContactPersonNote()
+            {
+                Id = 1,
+                ContactPerson = contactPerson,
+                Note = "Contact opgenomen met Niels over een inspectie. Voorstel volgt."
+            };
+
+            context.ContactPersonNotes.AddOrUpdate(note);
+
+            //var address = new Address
+            //{
+            //    Id = 3,
+            //    Country = "Duitsland",
+            //    StreetName = "Flughafen-Ring",
+            //    HouseNumber = 16,
+            //    City = "Weeze",
+            //    ZipCode = "NW47652"
+            //};
+
+            //context.Addresses.AddOrUpdate(address);
+            //var festival = new Festival
+            //{
+            //    Id = 1,
+            //    FestivalName = "Q-BASE",
+            //    Customer = customer,
+            //    Description = "Nachtfestival over de grens",
+            //    Address = address
+            //};
+
+            //context.Festivals.AddOrUpdate(festival);
+
+
+            //var openingHours = new OpeningHours
+            //{
+            //    Id = 1,
+            //    StartTime = new DateTime(2020, 9, 5, 18, 0, 0),
+            //    EndTime = new DateTime(2020, 9, 6, 8, 0, 0),
+            //    Festival = festival
+            //};
+
+            //context.OpeningHours.AddOrUpdate(openingHours);
+
+            //customer.Festivals = new List<Festival>
+            //{
+            //    festival
+            //};
+
+            //context.Customers.AddOrUpdate(customer);
+
+            //var questionnaire = new Questionnaire
+            //{
+            //    Id = 1,
+            //    Festival = festival,
+            //};
+
+            //var plannedInspection = new PlannedInspection
+            //{
+            //    Id = 1,
+            //    Employee = employee,
+            //    Festival = festival,
+            //    EventTitle = "Inspection " + festival.FestivalName,
+            //    StartTime = new DateTime(2020, 7, 28, 20, 00, 00),
+            //    EndTime = new DateTime(2020, 7, 29, 5, 00, 00),
+            //    Questionnaire = questionnaire
+            //};
+
+            //context.PlannedInspections.AddOrUpdate(plannedInspection);
+
+            context.SaveChanges();
+        }
+
+        private static Employee CreateEmployee(FestispecContext context)
+        {
+            var employee = new Employee()
+            {
                 Id = 1,
                 Iban = "NL01RABO1234567890",
                 Name = new FullName()
@@ -68,133 +197,7 @@ namespace Festispec.Models.Migrations
 
             context.Employees.AddOrUpdate(employee);
 
-            context.Customers.AddOrUpdate(new Customer()
-            {
-                Id = 1,
-                CustomerName = "Q-DANCE",
-                KvkNr = 34212891,
-                Address = new Address {
-                    Id = 2,
-                    StreetName = "Isolatorweg",
-                    HouseNumber = 36,
-                    ZipCode = "1014AS",
-                    City = "Amsterdam",
-                    Country = "Nederland"
-                },
-                ContactDetails = new ContactDetails
-                {
-                    Id = 2,
-                    EmailAddress = "info@q-dance.com",
-                    PhoneNumber = "+31204877300"
-                }
-            });
-
-            var contactPerson = new ContactPerson()
-            {
-                Id = 1,
-                Customer = new Customer()
-                {
-                    Id = 1,
-                    CustomerName = "Q-DANCE",
-                    KvkNr = 34212891,
-                    Address = new Address {
-                        Id = 2,
-                        StreetName = "Isolatorweg",
-                        HouseNumber = 36,
-                        ZipCode = "1014AS",
-                        City = "Amsterdam",
-                        Country = "Nederland"
-                    },
-                    ContactDetails = new ContactDetails
-                    {
-                        Id = 2,
-                        EmailAddress = "info@q-dance.com",
-                        PhoneNumber = "+31204877300"
-                    }
-                },
-                Name = new FullName
-                {
-                    Id = 2,
-                    First = "Niels",
-                    Last = "Kijf"
-                },
-                ContactDetails = new ContactDetails
-                {
-                    Id = 3,
-
-                    // fake news
-                    EmailAddress = "nielskijf@q-dance.com"
-                },
-                Role = "MA"
-            };
-
-            context.ContactPersons.AddOrUpdate(contactPerson);
-
-            var note = new ContactPersonNote()
-            {
-                Id = 1,
-                ContactPerson = contactPerson,
-                Note = "Contact opgenomen met Niels over een inspectie. Voorstel volgt."
-            };
-
-            context.ContactPersonNotes.AddOrUpdate(note);
-
-            var festival = new Festival
-            {
-                Id = 1,
-                FestivalName = "Q-BASE",
-                Description = "Nachtfestival over de grens",
-                Address = new Address
-                {
-                    Id = 3,
-                    Country = "Duitsland",
-                    StreetName = "Flughafen-Ring",
-                    HouseNumber = 16,
-                    City = "Weeze",
-                    ZipCode = "NW47652"
-                }
-            };
-
-            context.Festivals.AddOrUpdate(festival);
-
-
-            var openingHours = new OpeningHours
-            {
-                Id = 1,
-                StartTime = new DateTime(2020, 9, 5, 18, 0, 0),
-                EndTime = new DateTime(2020, 9, 6, 8, 0, 0),
-                Festival = festival
-            };
-
-            context.OpeningHours.AddOrUpdate(openingHours);
-
-            customer.Festivals = new List<Festival>
-            {
-                festival
-            };
-
-            context.Customers.AddOrUpdate(customer);
-
-            var questionnaire = new Questionnaire
-            {
-                Id = 1,
-                Festival = festival,
-            };
-
-            var plannedInspection = new PlannedInspection
-            {
-                Id = 1,
-                Employee = employee,
-                Festival = festival,
-                EventTitle = "Inspection " + festival.FestivalName,
-                StartTime = new DateTime(2020, 7, 28, 20, 00, 00),
-                EndTime = new DateTime(2020, 7, 29, 5, 00, 00),
-                Questionnaire = questionnaire
-            };
-
-            context.PlannedInspections.AddOrUpdate(plannedInspection);
-
-            context.SaveChanges();
+            return employee;
         }
     }
 }
