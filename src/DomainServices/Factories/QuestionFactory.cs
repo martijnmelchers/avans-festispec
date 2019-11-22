ec7418bfc75662c99d3e3f217bf94dba27f9fc7b
ThaDennuz <dgghaare@avans.nl>
2019-11-22 22:40:01 +0100

    Deleted unneccessary dictionary in factory and deleted old factory

--- src/DomainServices/Factories/QuestionFactory.cs
+++ src/DomainServices/Factories/QuestionFactory.cs
@@ -7,42 +7,35 @@ namespace Festispec.Models.Factories
 {
     public class QuestionFactory
     {
-        public IEnumerable<string> QuestionTypes => _questions.Keys;
+        public IEnumerable<string> QuestionTypes { get; }
 
-        private readonly Dictionary<string, Question> _questions;
         public QuestionFactory()
         {
-            _questions = new Dictionary<string, Question>
+            QuestionTypes = new List<String>()
             {
-                ["Tekenvraag"] = new DrawQuestion(),
-                ["Open vraag"] = new StringQuestion(),
-                ["Afbeeldingsvraag"] = new UploadPictureQuestion(),
-                ["Beoordelingsvraag"] = new RatingQuestion(),
-                ["Meerkeuzevraag"] = new MultipleChoiceQuestion(),
-                ["Numerieke vraag"] = new NumericQuestion(),
-                ["Referentievraag"] = new ReferenceQuestion()
+                "Tekenvraag",
+                "Open vraag",
+                "Afbeeldingsvraag",
+                "Beoordelingsvraag",
+                "Meerkeuzevraag",
+                "Numerieke vraag",
+                "Referentievraag"
             };
+
         }
         public Question GetQuestionType(string type)
         {
-            switch (type)
+            return type switch
             {
-                case "Tekenvraag":
-                    return new DrawQuestion();
-                case "Open vraag":
-                    return new StringQuestion();
-                case "Afbeeldingsvraag":
-                    return new UploadPictureQuestion();
-                case "Beoordelingsvraag":
-                    return new RatingQuestion();
-                case "Meerkeuzevraag":
-                    return new MultipleChoiceQuestion();
-                case "Numerieke vraag":
-                    return new NumericQuestion();
-                case "Referentievraag":
-                    return new ReferenceQuestion();
-            }
-            return null;
+                "Tekenvraag" => new DrawQuestion(),
+                "Open vraag" => new StringQuestion(),
+                "Afbeeldingsvraag" => new UploadPictureQuestion(),
+                "Beoordelingsvraag" => new RatingQuestion(),
+                "Meerkeuzevraag" => new MultipleChoiceQuestion(),
+                "Numerieke vraag" => new NumericQuestion(),
+                "Referentievraag" => new ReferenceQuestion(),
+                _ => null
+            };
         }
     }
 }
