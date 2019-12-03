24d76744a1d5a6d7d7aab1038267ed0945e98242
NanoSector <rick@nanosector.nl>
2019-12-03 15:56:49 +0100

    Add CreateCustomer overload
    Signed-off-by: NanoSector <rick@nanosector.nl>

--- src/DomainServices/Interfaces/ICustomerService.cs
+++ src/DomainServices/Interfaces/ICustomerService.cs
@@ -7,9 +7,10 @@ namespace Festispec.DomainServices.Interfaces
     public interface ICustomerService
     {
         List<Customer> GetAllCustomers();
-        Task<Customer> CreateCustomer(string name, int kvkNr, Address address, ContactDetails contactDetails);
         Customer GetCustomer(int customerId);
         Task RemoveCustomer(int customerId);
         Task SaveChanges();
+        Task<Customer> CreateCustomer(string name, int kvkNr, Address address, ContactDetails contactDetails);
+        Task<Customer> CreateCustomer(Customer customer);
     }
 }
