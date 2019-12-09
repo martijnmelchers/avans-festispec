b8dcaa59d004f1506040c4fee5e08d05736c8b0d
stijntoemen <stijntoemen@gmail.com>
2019-12-09 13:43:27 +0100

    oke

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Web.Models;
+using Festispec.Web.Models;
 
 namespace Festispec.Web.Controllers
 {
