1c9e85010321554abf88d6fcc25020316116d035
NanoSector <rick@nanosector.nl>
2019-12-17 17:08:58 +0100

    Wire up EmployeePage
    Signed-off-by: NanoSector <rick@nanosector.nl>

--- src/UserInterface/Views/Employee/EmployeePage.xaml.cs
+++ src/UserInterface/Views/Employee/EmployeePage.xaml.cs
@@ -1,11 +1,11 @@
 ﻿using System.Windows.Controls;
-using Festispec.UI.ViewModels.Customers;
+using Festispec.UI.ViewModels.Employees;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Festispec.UI.Views.Employee
 {
     /// <summary>
-    /// Interaction logic for CustomerInformationScreen.xaml
+    /// Interaction logic for EmployeeInformationScreen.xaml
     /// </summary>
     public partial class EmployeePage : Page
     {
@@ -16,7 +16,7 @@ namespace Festispec.UI.Views.Employee
             IServiceScope scope = AppServices.Instance.ServiceProvider.CreateScope();
             Unloaded += (sender, e) => scope.Dispose();
 
-            DataContext = scope.ServiceProvider.GetRequiredService<CustomerViewModel>();
+            DataContext = scope.ServiceProvider.GetRequiredService<EmployeeViewModel>();
         }
 
 
