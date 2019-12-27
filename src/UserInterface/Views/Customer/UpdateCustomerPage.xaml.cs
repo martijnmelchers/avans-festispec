176e25346462a0dbd72bb4432079b12a9ec984c1
NanoSector <rick@nanosector.nl>
2019-12-27 21:47:27 +0100

    Clean up customer page code-behind
    Signed-off-by: NanoSector <rick@nanosector.nl>

--- src/UserInterface/Views/Customer/UpdateCustomerPage.xaml.cs
+++ src/UserInterface/Views/Customer/UpdateCustomerPage.xaml.cs
@@ -1,19 +1,10 @@
-﻿using System.Text.RegularExpressions;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using Festispec.UI.ViewModels.Customers;
+﻿using Festispec.UI.ViewModels.Customers;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Festispec.UI.Views.Customer
 {
-    /// <summary>
-    /// Interaction logic for EditCustomerPage.xaml
-    /// </summary>
-    public partial class UpdateCustomerPage : Page
+    public partial class UpdateCustomerPage
     {
-        private static readonly Regex NumericOnlyRegex = new Regex("[^0-9]+");
-        
         public UpdateCustomerPage()
         {
             InitializeComponent();
@@ -23,11 +14,5 @@ namespace Festispec.UI.Views.Customer
 
             DataContext = scope.ServiceProvider.GetRequiredService<CustomerViewModel>();
         }
-
-        private void NumericTextBlockOnPreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            e.Handled = NumericOnlyRegex.IsMatch(e.Text);
-        }
-
     }
 }
