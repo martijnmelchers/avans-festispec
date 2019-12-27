ec1175886c4813f95e8cbc4b3d1f96dd72e13bb6
Thedaniel23 <43467427+Thedaniel23@users.noreply.github.com>
2019-12-27 11:02:51 +0100

    Added page

---
+++ src/Web/Controllers/SicknessController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Festispec.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class SicknessController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Availability avalabillity)
+        {
+            return View();
+        }
+    }
+}
