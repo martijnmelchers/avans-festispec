a4e88e8f72c633e3c1898e548cf2003c20d49b61
ThaDennuz <dgghaare@avans.nl>
2019-12-30 03:34:14 +0100

    Created availabilityservice
    Signed-off-by: ThaDennuz <dgghaare@avans.nl>

---
+++ src/DomainServices/Interfaces/IAvailabilityService.cs
@@ -0,0 +1,23 @@
+﻿using Festispec.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festispec.DomainServices.Interfaces
+{
+    public interface IAvailabilityService
+    {
+        Task<Availability> AddUnavailabilityEntireDay(int employeeId, DateTime date, string reason);
+
+        Task<Availability> AddUnavailabilityPartOfDay(int employeeId, DateTime beginDateTime, DateTime endDateTime, string reason);
+
+        Task RemoveUnavailablity(int availabilityId);
+
+        Task SaveChanges();
+
+        Dictionary<int, List<Availability>> GetUnavailabilitiesForMonth(int employeeId, int month, int year);
+
+        List<Availability> GetUnavailabilitiesForDay(int employeeId, DateTime date);
+    }
+}
---
+++ src/DomainServices/Services/AvailabilityService.cs
@@ -0,0 +1,120 @@
+﻿using Festispec.DomainServices.Interfaces;
+using Festispec.Models;
+using Festispec.Models.EntityMapping;
+using Festispec.Models.Exception;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festispec.DomainServices.Services
+{
+    public class AvailabilityService : IAvailabilityService
+    {
+        private readonly FestispecContext _db;
+
+        public AvailabilityService(FestispecContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Availability> AddUnavailabilityEntireDay(int employeeId, DateTime date, string reason)
+        {
+            if (date < DateTime.Now)
+                throw new DateHasPassedException();
+
+            var employee = _db.Employees.FirstOrDefault(e => e.Id == employeeId);
+
+            var availability = new Availability()
+            {
+                IsAvailable = false,
+                Employee = employee,
+                StartTime = new DateTime(date.Year, date.Month, date.Day, 0, 0, 0),
+                EndTime = new DateTime(date.Year, date.Month, date.Day, 23, 59, 59),
+                Reason = reason,
+                EventTitle = "Niet beschikbaar"
+            };
+
+            if (!availability.Validate())
+                throw new InvalidDataException();
+
+            _db.PlannedEvents.Add(availability);
+
+            if (await _db.SaveChangesAsync() == 0)
+                throw new NoRowsChangedException();
+
+            return availability;
+        }
+
+        public async Task<Availability> AddUnavailabilityPartOfDay(int employeeId, DateTime beginDateTime, DateTime endDateTime, string reason)
+        {
+            if (beginDateTime < DateTime.Now || endDateTime < DateTime.Now)
+                throw new DateHasPassedException();
+
+            if (endDateTime < beginDateTime)
+                throw new EndDateEarlierThanStartDateException();
+
+            if (beginDateTime.Date != endDateTime.Date)
+                throw new StartAndEndDateDifferentDaysException();
+
+            var employee = _db.Employees.FirstOrDefault(e => e.Id == employeeId);
+
+            var availability = new Availability()
+            {
+                IsAvailable = false,
+                Employee = employee,
+                StartTime = beginDateTime,
+                EndTime = endDateTime,
+                Reason = reason,
+                EventTitle = "Niet beschikbaar"
+            };
+
+            if (!availability.Validate())
+                throw new InvalidDataException();
+
+            _db.PlannedEvents.Add(availability);
+
+            if (await _db.SaveChangesAsync() == 0)
+                throw new NoRowsChangedException();
+
+            return availability;
+        }
+
+        public List<Availability> GetUnavailabilitiesForDay(int employeeId, DateTime date)
+        {
+            var availabilities = _db.Availabilities.Where(a => a.Employee.Id == employeeId && a.StartTime.Date == date.Date && a.EventTitle == "Niet beschikbaar").ToList();
+
+            return availabilities;
+        }
+
+        public Dictionary<int, List<Availability>> GetUnavailabilitiesForMonth(int employeeId, int month, int year)
+        {
+            if (month > 12)
+                throw new InvalidDataException();
+
+            Dictionary<int, List<Availability>> availabilities = new Dictionary<int, List<Availability>>();
+
+            for (int x = 1; x <= 31; x++)
+            {
+                availabilities.Add(x, GetUnavailabilitiesForDay(employeeId, new DateTime(year, month, x)));
+            }
+
+            return availabilities;
+        }
+
+        public async Task RemoveUnavailablity(int availabilityId)
+        {
+            var availability = _db.Availabilities.FirstOrDefault(a => a.Id == availabilityId);
+
+            _db.PlannedEvents.Remove(availability);
+
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task SaveChanges()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
