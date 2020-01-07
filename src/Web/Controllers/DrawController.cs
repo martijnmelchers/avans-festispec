ad832a712f92c910cff3c2a09a98d752caad82c6
stijntoemen <stijntoemen@gmail.com>
2020-01-07 16:35:08 +0100

    fixed draw question

--- src/Web/Controllers/DrawController.cs
+++ src/Web/Controllers/DrawController.cs
@@ -7,6 +7,7 @@ using Festispec.DomainServices.Interfaces;
 using Festispec.Models.Answers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace Festispec.Web.Controllers
 {
@@ -30,27 +31,69 @@ namespace Festispec.Web.Controllers
         [HttpPost]
         public async Task<ActionResult> Draw(string imageData)
         {
-            var request = Request.Form;
-            FileAnswer fileAnswer = _questionnaireService.getAnswers().FirstOrDefault(e => e.Id == 1) as FileAnswer;
+            JObject j = JObject.Parse(imageData);
+            int questionId = int.Parse(j["QuestionId"].ToString());
+            FileAnswer fileAnswer = _questionnaireService.getAnswers().FirstOrDefault(e => e.Id == questionId) as FileAnswer;
 
-            var path = "test";
+            fileAnswer.Question = _questionnaireService.GetPlannedInspections().FirstOrDefault(e => e.Id == fileAnswer.PlannedInspection.Id).Questionnaire.Questions.FirstOrDefault(e => e.Id == questionId);
+            fileAnswer.PlannedInspection = _questionnaireService.GetPlannedInspections(fileAnswer.PlannedInspection.Id);
 
-            string fileNameWitPath = path + DateTime.Now.ToString().Replace("/", "-").Replace(" ", "- ").Replace(":", "") + ".png";
-            using (FileStream fs = new FileStream(fileNameWitPath, FileMode.Create))
+            imageData = j["ImageData"].ToString();
+
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\Uploads", DateTime.Now.ToString().Replace("/", "-").Replace(" ", "- ").Replace(":", "") + ".png");
+
+            var bytes = Convert.FromBase64String(imageData);
+            using (var imageFile = new FileStream(filePath, FileMode.Create))
             {
-                using (BinaryWriter bw = new BinaryWriter(fs))
-                {
-                    byte[] data = Convert.FromBase64String(imageData);
-                    bw.Write(data);
-                    bw.Close();
-                }
+                imageFile.Write(bytes, 0, bytes.Length);
+                imageFile.Flush();
             }
 
+            if (fileAnswer.Id != 0)
+                (_questionnaireService.getAnswers().FirstOrDefault(e => e.Id == fileAnswer.Id) as FileAnswer).UploadedFilePath = fileAnswer.UploadedFilePath;
+            else await _questionnaireService.CreateAnswer(fileAnswer);
+            await _questionnaireService.SaveChangesAsync();
 
-            // process uploaded files
-            // Don't rely on or trust the FileName property without validation.
+            return RedirectToAction("Details", "inspection", new { id = fileAnswer.PlannedInspection.Id });
 
-            return Ok(new { V = "test" });
+
+
+
+
+        }
+
+
+        public async Task<ActionResult> SaveFileAnswer(IFormFile file)
+        {
+            FileAnswer fileAnswer = new FileAnswer();
+            int questionId = int.Parse(Request.Form["QuestionId"].ToString());
+            fileAnswer.PlannedInspection = _questionnaireService.GetPlannedInspections(int.Parse(Request.Form["PlannedInspectionId"].ToString()));
+            fileAnswer.Question = _questionnaireService.GetPlannedInspections().FirstOrDefault(e => e.Id == fileAnswer.PlannedInspection.Id).Questionnaire.Questions.FirstOrDefault(e => e.Id == questionId);
+
+            var filePath = await UploadFile(file);
+            fileAnswer.UploadedFilePath = filePath;
+
+            if (fileAnswer.Id != 0)
+                (_questionnaireService.getAnswers().FirstOrDefault(e => e.Id == fileAnswer.Id) as FileAnswer).UploadedFilePath = fileAnswer.UploadedFilePath;
+            else await _questionnaireService.CreateAnswer(fileAnswer);
+            await _questionnaireService.SaveChangesAsync();
+            return RedirectToAction("Details", new { id = fileAnswer.PlannedInspection.Id });
+        }
+
+        private async Task<string> UploadFile(IFormFile ufile)
+        {
+            if (ufile != null && ufile.Length > 0)
+            {
+                var fileName = Path.GetFileName(ufile.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\Uploads", fileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await ufile.CopyToAsync(fileStream);
+                }
+                return filePath;
+            }
+            return null;
         }
 
 
